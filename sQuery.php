e150969ea7716a75c043e717ae930d6fdae559b8
ucscode <34024404+ucscode@users.noreply.github.com>
2022-11-16 09:31:13 +0100

    v1.1

--- sQuery.php
+++ sQuery.php
@@ -4,33 +4,48 @@
 
 class sQuery {
 	
+	protected static $mysqli;
+	
+	public static function connect( MYSQLI $mysqli ) {
+		self::$mysqli = $mysqli;
+	}
+	
 	public static function select( $tablename, $condition = 1 ) {
 		$SQL = "SELECT * FROM `{$tablename}` WHERE {$condition}";
 		return $SQL;
 	}
 	
-	public static function val( $value ) {
+	private static function val( $value ) {
 		if( is_numeric($value) ) return $value;
 		else if( is_null($value) ) return 'NULL';
 		else return "'{$value}'";
 	}
 	
 	public static function insert( string $tablename, array $data ) {
-		$columns = implode(",", array_keys($data));
+		$columns = implode(", ", array_map(function($key) {
+			return "`{$key}`";
+		}, array_keys($data)));
 		$values = array_map(function($value) {
+			if( self::$mysqli ) $value = self::$mysqli->real_escape_string( $value );
 			return self::val( $value );
 		}, array_values($data));
-		$values = implode(",", $values);
+		$values = implode(", ", $values);
 		$SQL = "INSERT INTO `{$tablename}` ($columns) VALUES ($values)";
 		return $SQL;
 	}
 	
 	public static function update( string $tablename, array $data, $condition = 1 ) {
 		$fieldset = array_map(function($key, $value) {
-			return "{$key} = " . self::val( $value );
+			if( self::$mysqli ) $value = self::$mysqli->real_escape_string( $value );
+			return "`{$key}` = " . self::val( $value );
 		}, array_keys($data), array_values($data));
 		$fieldset = implode(", ", $fieldset);
 		$SQL = "UPDATE `{$tablename}` SET {$fieldset} WHERE {$condition}";
+		return $SQL;
+	}
+	
+	public static function delete( string $tablename, string $condition ) {
+		$SQL = "DELETE FROM `{$tablename}` WHERE {$condition}";
 		return $SQL;
 	}
 	
