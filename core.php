30a9e01030deebd38c0972e184a4f395c4eb0ac0
ucscode <34024404+ucscode@users.noreply.github.com>
2022-12-10 00:09:58 -0800

    v1.0.4

--- core.php
+++ core.php
@@ -2,13 +2,18 @@
 
 class core {
 
-	# [ CONVERT BACKSLASH TO FORWARD SLASH ]
+	/*
+		--- [ CONVERT BACKSLASH TO FORWARD SLASH ]
+	*/
 	
 	public static function rslash( $PATH ) {
 		return str_replace("\\", "/", $PATH);
 	}
 	
-	# [ CONVERT SERVER PATH TO URL ] 
+	
+	/* 
+		--- [ CONVERT SERVER PATH TO URL ] 
+	*/
 	
 	public static function url( $DOC_ROOT = ROOT_DIR, $MINI = FALSE ) {
 		$DOC_ROOT = self::rslash( $DOC_ROOT );
@@ -17,7 +22,10 @@ class core {
 		return (!$MINI ? ($SCHEME . "://") : '/') . $SRV_URL;
 	}
 	
-	# [ GET REQUEST_URI ]
+	
+	/* 
+		--- [ GET REQUEST_URI ]
+	*/
 	
 	public static function request_uri() {
 		$FULL_REQUEST_URI = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
@@ -26,7 +34,10 @@ class core {
 		return $REQUEST_URI;
 	}
 	
-	# [ ARRAY TO ATTR ] 
+	
+	/* 
+		--- [ ARRAY TO ATTR ] 
+	*/
 	
 	public static function array_to_html_attrs( array $array ) {
 		return implode(" ", array_map(function($key, $value) {
@@ -35,7 +46,10 @@ class core {
 		}, array_keys($array), array_values($array)));
 	}
 	
-	# [ GENERATE KEY ]
+	
+	/* 
+		--- [ GENERATE KEY ]
+	*/
 	
 	public static function keygen($length = 10, bool $use_spec_char = false) {
 		$data = array( ...range(0, 9), ...range('a', 'z'), ...range('A', 'Z') );
@@ -50,5 +64,58 @@ class core {
 		};
 		return $key;
 	}
+	
+	
+	/* 
+		--- [ REPLACE %{var} WITH ARRAY VALUE ] 
+	*/
+	
+	public static function replace_var( string $string, array $data ) {
+		$new_string = preg_replace_callback( "~%\{([^\}]+)\}~", function( $match ) use($data) {
+			$key = $match[1];
+			return $data[ $key ] ?? $match[0];
+		}, $string );
+		return $new_string;
+	}
+	
+	
+	/* 
+		--- [ GET STRONG REGULAR EXPRESSION ]
+	*/
+	
+	public static function regex( string $name, $strict = false ) {
+		if( $strict )  {
+			$BEGIN = '^';
+			$END = '$';
+		} else $BEGIN = $END = NULL;
+		## ----- Create REGEX ------
+		switch( strtoupper($name) ) {
+			case 'EMAIL':	
+				return '/' . $BEGIN . '(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))' . $END . '/';
+			case "URL":
+				return "/{$BEGIN}(?:https?:\/\/)?(?:[\w.-]+(?:(?:\.[\w\.-]+)+)|(?:localhost(:\d{1,4})?\/))[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.%]+{$END}/i";
+			case "NUMBER":
+				return "/{$BEGIN}\-?\d+(?:\.\d+)?{$END}/";
+			case "DATE":
+				return "/{$BEGIN}(0[1-9]|[1-2][0-9]|3[0-1])(?:\-|\/)(0[1-9]|1[0-2])(?:\-|\/)[0-9]{4}{$END}/i";
+			case "BTC":
+				$regex = "/{$BEGIN}[13][a-km-zA-HJ-NP-Z0-9]{26,33}{$END}/i";
+				break;
+		}
+	}
 
+	
+	/*
+		--- [ CHECK IF PHP NAMESPACE EXISTS ]
+	*/
+	
+	public function namespace_exists($namespace) {
+		// credit to stackoverflow
+		$namespace .= '\\';
+		foreach( get_declared_classes() as $classname )
+			if( strpos($classname, $namespace) === 0 ) return true;
+		return false;
+	}
+	
+	
 }
